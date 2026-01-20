833fdf3c81f8e09cda6cf931c13cf8326e47340d
Maciej Chodkowski <maciej.chodkowski@ls.lan>
2026-01-20 09:52:18 +0100

    z poradnika christian

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'qc-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './html2pdf.bundle.min.js',
-  './manifest.json',
-  './icon.png',
-  './icon-512.png'
+const cacheName = "qc-assistant-v1";
+const assetsToCache = [
+  "/PWA_QC/",
+  "/PWA_QC/index.html",
+  "/PWA_QC/manifest.webmanifest",
+  "/PWA_QC/icon-192.png",
+  "/PWA_QC/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
