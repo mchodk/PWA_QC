86586d01bffe1cf2d30baae383564eb5c030700c
Maciej Chodkowski <maciej.chodkowski@ls.lan>
2026-01-20 09:08:18 +0100

    poprawki gpt

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
 const CACHE_NAME = 'qc-cache-v1';
 const urlsToCache = [
-  'index.html',
-  'html2pdf.bundle.min.js',
-  'manifest.json',
-  'icon.png',
-  'icon-512.png'
+  './index.html',
+  './html2pdf.bundle.min.js',
+  './manifest.json',
+  './icon.png',
+  './icon-512.png'
 ];
 
-// Instalacja Service Worker i cache
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch – zwracanie z cache jeśli offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
